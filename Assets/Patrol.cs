e46ce51aa80b148715f6372e0c09e7f855819dd9
ggreiner01 <ggreiner1995@aol.com>
2018-11-28 14:49:20 -0600

    patient object moves

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -6,37 +6,73 @@ using System.Collections;
 
 public class Patrol : MonoBehaviour
 {
+    Animator anim;
     private bool visible = false;
     private Vector3 pos1;
     private Vector3 pos2;
-    public Vector3 posDiff = new Vector3(0f, 0f, 20f);
+    public Vector3 posDiff = new Vector3(0f, 0f, 5f);
     public float speed = 1.0f;
+    public string TagGiven = "";
+    public Transform target;
+    public Transform Player;
     void Start()
     {
-
+        anim = GetComponent<Animator>();
+       
         pos1 = transform.position;
         pos2 = transform.position + posDiff;
     }
-    void OnBecameVisible()
-    {
-        visible = true;
-    }
-
-    // OnBecameInvisible is called when the renderer is no longer visible by any camera. INCLUDING YOUR EDITOR CAMERA
-    void OnBecameInvisible()
-    {
-        visible = false;
-    }
-
     void Update()
     {
-        if (!visible)
+        float dist = Vector3.Distance(transform.position, Player.position);
+        if (gameObject.tag == "Adult")
+        {
+            TagGiven = gameObject.GetComponent<AdultObject>().GetTagGussed();
+
+        }
+        else if (gameObject.tag == "Child")
+        {
+            TagGiven = gameObject.GetComponent<ChildObject>().GetTagGussed();
+
+        }
+        if (dist >= 3 && TagGiven != "Green")
             move();
+        else if (dist <= 3 && TagGiven != "Green")
+        {
+            anim.Play("HumanoidIdle");
+            transform.LookAt(Player.position);
+        }
+        else if (TagGiven == "Green" && transform.position != target.position)
+            goTowards();
+        else if (transform.position == target.position)
+            anim.Play("HumanoidIdle");
+        
     }
 
     public void move()
     {
+        anim.Play("Run");
         transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
-        transform.Rotate(Vector3.up * 10f * Time.deltaTime);
+        float dist1 = Vector3.Distance(pos1, transform.position);
+        float dist2 = Vector3.Distance(transform.position, pos2);
+        if (dist1 < 0.8)
+        {
+            transform.LookAt(pos2);
+        }
+        if (dist2 < 0.8)
+        {
+            transform.LookAt(pos1);
+        }
+    }
+    public void goTowards()
+    {
+        anim.Play("Run");
+        transform.LookAt(target.position);
+        float step = 2.0f * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
     }
 }
